f59ad61a5432eaa0a1a77f4042deeec6e9960cb5
yshir <yshir0020@gmail.com>
2021-06-10 01:52:45 +0900

    handle keys, move, exit

--- src/main.rs
+++ src/main.rs
@@ -22,11 +22,49 @@ fn main() {
 
     let mut tcod = Tcod { root };
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !tcod.root.window_closed() {
         tcod.root.set_default_foreground(WHITE);
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root
+            .put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
+
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
     }
+}
+
+/**
+ * true: exit game
+ * false: continue game
+ */
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            tcod.root.set_fullscreen(!tcod.root.is_fullscreen());
+        }
+        Key { code: Escape, .. } => return true,
+
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+
+        _ => {}
+    }
+
+    false
 }
