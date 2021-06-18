2c7138b6bf7955df602211278ef0e56a606a5cdd
yshir <yshir0020@gmail.com>
2021-06-19 01:26:39 +0900

    feat: main menu

--- src/main.rs
+++ src/main.rs
@@ -345,8 +345,43 @@ fn main() {
         mouse: Default::default(),
     };
 
-    let (mut game, mut objects) = new_game(&mut tcod);
-    play_game(&mut tcod, &mut game, &mut objects);
+    main_menu(&mut tcod);
+}
+
+fn main_menu(tcod: &mut Tcod) {
+    let img =
+        tcod::image::Image::from_file("menu_background.png").expect("Background image not found");
+
+    while !tcod.root.window_closed() {
+        // show the background image, at twice the regular console resolution
+        tcod::image::blit_2x(&img, (0, 0), (-1, -1), &mut tcod.root, (0, 0));
+
+        tcod.root.set_default_foreground(LIGHT_YELLOW);
+        tcod.root.print_ex(
+            SCREEN_WIDTH / 2,
+            SCREEN_HEIGHT / 2 - 4,
+            BackgroundFlag::None,
+            TextAlignment::Center,
+            "TOMBS OF THE ANCIENT KINGS",
+        );
+
+        // show options and wait for the player's choice
+        let choices = &["Play a new game", "Continue last game", "Quit"];
+        let choice = menu("", choices, 24, &mut tcod.root);
+
+        match choice {
+            Some(0) => {
+                // new game
+                let (mut game, mut objects) = new_game(tcod);
+                play_game(tcod, &mut game, &mut objects);
+            }
+            Some(2) => {
+                // quit
+                break;
+            }
+            _ => {}
+        }
+    }
 }
 
 fn new_game(tcod: &mut Tcod) -> (Game, Vec<Object>) {
@@ -393,6 +428,9 @@ fn initialize_fov(tcod: &mut Tcod, map: &Map) {
             );
         }
     }
+
+    // unexplored areas start black (which is the default background color)
+    tcod.con.clear();
 }
 
 fn play_game(tcod: &mut Tcod, game: &mut Game, objects: &mut Vec<Object>) {
@@ -981,7 +1019,11 @@ fn menu<T: AsRef<str>>(header: &str, options: &[T], width: i32, root: &mut Root)
     );
 
     // calculate total height for the header (after auto-wrap) and one line per option
-    let header_height = root.get_height_rect(0, 0, width, SCREEN_HEIGHT, header);
+    let header_height = if header.is_empty() {
+        0
+    } else {
+        root.get_height_rect(0, 0, width, SCREEN_HEIGHT, header)
+    };
     let height = options.len() as i32 + header_height;
 
     // create an off-screen console that represents the menu's window
